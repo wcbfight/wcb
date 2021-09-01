9d81e09bdda19b8767f3ea5a23a8d3c27361f338
wangchengbin <wangchengb@chinatelecom.cn>
2021-09-01 16:23:36 +0800

    feat: 页面布局完成，但是响应式未完成，同时字体大小和某些边距需要确认

--- helper.js
+++ helper.js
@@ -1,8 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2021-08-31 15:55:36
- * @LastEditTime: 2021-08-31 15:55:37
+ * @LastEditTime: 2021-09-01 14:45:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \新建文件夹\helper\helper.js
  */
+function topHead(){
+  console.log(document.body.scrollTop)
+}
